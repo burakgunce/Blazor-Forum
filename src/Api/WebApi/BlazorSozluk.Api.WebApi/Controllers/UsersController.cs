cee1b6771fbdc259844e1f86d85ae39318927aa2
burakgunce <brk.gnc9@hotmail.com>
2024-03-19 20:07:51 +0300

    automapper has been added and user login structure has been prepared

---
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using BlazorSozluk.Common.Models.Queries;
+using BlazorSozluk.Common.Models.RequestModels;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorSozluk.Api.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginUserCommand command)
+        {
+            LoginUserViewModel response = await _mediator.Send(command);
+            return Ok(response);
+        }
+    }
+}
