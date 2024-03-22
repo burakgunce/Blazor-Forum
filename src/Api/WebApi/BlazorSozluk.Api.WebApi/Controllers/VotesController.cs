41af69b3f0d1b46091461f548ab1e66d4a6c014e
burakgunce <brk.gnc9@hotmail.com>
2024-03-22 15:56:28 +0300

    vote controller has been added

---
+++ src/Api/WebApi/BlazorSozluk.Api.WebApi/Controllers/VotesController.cs
@@ -0,0 +1,58 @@
+﻿using BlazorSozluk.Api.Application.Features.Commands.Entry.DeleteVote;
+using BlazorSozluk.Api.Application.Features.Commands.EntryComment.DeleteVote;
+using BlazorSozluk.Common.Models.RequestModels;
+using BlazorSozluk.Common.ViewModels;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorSozluk.Api.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VotesController : BaseController
+    {
+        private readonly IMediator _mediator;
+
+        public VotesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("Entry/{entryId}")]
+        public async Task<IActionResult> CreateEntryVote(Guid entryId, VoteType voteType = VoteType.UpVote)
+        {
+            var result = await _mediator.Send(new CreateEntryVoteCommand(entryId, voteType, UserId.Value));
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("EntryComment/{entryCommentId}")]
+        public async Task<IActionResult> CreateEntryCommentVote(Guid entryCommentId, VoteType voteType = VoteType.UpVote)
+        {
+            var result = await _mediator.Send(new CreateEntryCommentVoteCommand(entryCommentId, voteType, UserId.Value));
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("DeleteEntryVote/{entryId}")]
+        public async Task<IActionResult> DeleteEntryVote(Guid entryId)
+        {
+            await _mediator.Send(new DeleteEntryVoteCommand(entryId, UserId.Value));
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("DeleteEntryCommentVote/{entryId}")]
+        public async Task<IActionResult> DeleteEntryCommentVote(Guid entryCommentId)
+        {
+            await _mediator.Send(new DeleteEntryCommentVoteCommand(entryCommentId, UserId.Value));
+
+            return Ok();
+        }
+    }
+}
